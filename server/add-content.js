6c41dc274fec1386e8dc80a011161b9ccc15b281
Luis Herranz <luisherranz@Luiss-iMac.Home>
2015-06-15 15:31:28 +0200

    add more complex examples

--- server/add-content.js
+++ server/add-content.js
@@ -1,5 +1,6 @@
 Meteor.startup(function() {
   var herman, antoine;
+  var date = Date.now();
   if (!Authors.findOne()){
     herman = Authors.insert({
       name: "Herman Melville",
@@ -16,14 +17,43 @@ Meteor.startup(function() {
       desc: "Ishmael narrates the monomaniacal quest of Ahab, captain of the " +
       "whaler Pequod, for revenge on Moby Dick, a white whale which on a " +
       "previous voyage destroyed Ahab's ship and severed his leg at the knee.",
-      author: herman
+      author: herman,
+      tags: [
+        "adventure",
+        "classic"
+      ],
+      dates: {
+        created: (date - 10000),
+        published: date
+      }
     });
     Books.insert({
       name: "Le Petit Prince",
       desc: "The Little Prince is a poetic tale, with watercolour " +
       "illustrations by the author, in which a pilot stranded in the desert " +
       "meets a young prince fallen to Earth from a tiny asteroid.",
-      author: antoine
-    })
+      author: antoine,
+      tags: [
+        "classic"
+      ],
+      dates: {
+        created: (date - 10000),
+        published: date
+      }
+    });
+    Books.insert({
+      name: "Terre des Hommes",
+      desc: "Wind, Sand and Stars (French title: Terre des hommes) is a "+
+      " memoir by the French aristocrat aviator-writer Antoine de "+
+      "Saint-Exupéry, and a winner of several literary awards.",
+      author: antoine,
+      tags: [
+        "unkown"
+      ],
+      dates: {
+        created: (date - 10000),
+        published: date
+      }
+    });
   }
 });
--- server/publications.js
+++ server/publications.js
@@ -1,8 +1,10 @@
 // These publications are needed to be able to control the data in the client
 // side. Of course, a different approach is needed for private data.
-Meteor.publish("Books", function(params, options){
-  return Books.find(params, options);
-});
-Meteor.publish("Authors", function(params, options){
-  return Authors.find(params, options);
-});
+for (var i = 1; i < 10; i++) {
+  Meteor.publish("Books" + i, function(params, options){
+    return Books.find(params, options);
+  });
+  Meteor.publish("Authors" + i, function(params, options){
+    return Authors.find(params, options);
+  });
+}
